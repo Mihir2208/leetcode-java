5513378a687da8fc4117c74c6d5bddf6359c892b
Mihir2208 <118858022+Mihir2208@users.noreply.github.com>
2026-01-29 23:15:13 -0600

    Time: 12 ms (84.54%) | Memory: 24.5 MB (89.74%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        dic = {}
+        for n in nums:
+            
+            if n in dic: return True
+            else: dic[n] = 1
+        
+        return False    
+        
