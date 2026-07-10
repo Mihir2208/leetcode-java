d39b8b75bb6f87e9194155e07b23af0e85c44b24
Mihir2208 <118858022+Mihir2208@users.noreply.github.com>
2026-07-09 19:44:07 -0500

    Time: 23 ms (50.89%) | Memory: 14.5 MB (8.36%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+       if len(s) != len(t):
+        return False
+       
+       sd = {}
+       td = {}
+
+       for i in range(len(s)):
+        sd[s[i]] = sd.get(s[i],0)+1
+        td[t[i]] = td.get(t[i],0)+1
+        
+       return sd == td
+    
+
+
+        
