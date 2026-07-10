2bda3cbfeaf44440ca2c766903144fdf5df5d066
Mihir2208 <118858022+Mihir2208@users.noreply.github.com>
2026-07-10 17:18:23 -0500

    Time: 127 ms (24.00%) | Memory: 15.8 MB (34.78%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.py
@@ -3,8 +3,6 @@ class Solution(object):
         mp = {}
         for i in range(len(s)):
             mp[s[i]] = mp.get(s[i], 0)+1
-
-        print(mp)
         for i, char in enumerate(s):
             if mp[char] == 1:
                 return i
